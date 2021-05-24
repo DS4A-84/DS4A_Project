cd882a5e8140b9541c43320dbee5775dcb13f65f
Oscar Hendrick <lincoln607@gmail.com>
2021-05-23 21:59:09 -0400

    added reduce function

--- clean_bacon_data.py
+++ clean_bacon_data.py
@@ -4,23 +4,26 @@
 # Group 84
 # Clean Oracle of Bacon Json Blob data
 
-file1 = open('data/bacon.txt', 'r', encoding="utf8")
-Lines = file1.readlines()
+from functools import reduce
 
 
-blob_format_dict = {
-    "INFOBOX: ":"",
-    "=>":":",
-    "[[": "",
-    "]]":""
-}
+bacon_data_path = 'data/bacon.txt'
+def load_txt_data(filepath):
+    a = open(filepath, 'r', encoding="utf8")
+    return a.readlines()
 
+def movie_blob_to_dict(Lines):
+    dict = {
+        "INFOBOX: ":"",
+        "=>":":",
+        "[[": "",
+        "]]":""
+        }
 
+    for i in Lines:
+        i = reduce(lambda x, y: x.replace(y, dict[y]), dict, i)
+        print(i)
+        exit()
 
-for i in Lines:
-    i = for o, r in blob_format_dict.items():
-        i = i.replace(o, r)
-
-
-    print(i)
-    exit()
+ff = load_txt_data(bacon_data_path)
+rr = movie_blob_to_dict(ff)
