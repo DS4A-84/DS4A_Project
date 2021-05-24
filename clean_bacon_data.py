d51c671509ed0de8e45feda5ff64b6fb921a7e41
Oscar Hendrick <lincoln607@gmail.com>
2021-05-23 23:42:51 -0400

    Update clean_bacon_data.py
    added  clean function outputing results to text file

--- clean_bacon_data.py
+++ clean_bacon_data.py
@@ -8,11 +8,12 @@ from functools import reduce
 
 
 bacon_data_path = 'data/bacon.txt'
+output_path = 'data/clean_bacon_dicts.txt'
 def load_txt_data(filepath):
     a = open(filepath, 'r', encoding="utf8")
     return a.readlines()
 
-def movie_blob_to_dict(Lines):
+def write_clean_blob_to_txt(lines,filepath):
     dict = {
         "INFOBOX: ":"",
         "=>":":",
@@ -20,10 +21,20 @@ def movie_blob_to_dict(Lines):
         "]]":""
         }
 
-    for i in Lines:
-        i = reduce(lambda x, y: x.replace(y, dict[y]), dict, i)
-        print(i)
-        exit()
+    file = open(filepath,"a", encoding="utf8")
+    for i in lines:
+        if "INFOBOX:" in i  or '{"title' in i :
+            file.write(reduce(lambda x, y: x.replace(y, dict[y]), dict, i))
+    file.close()
 
-ff = load_txt_data(bacon_data_path)
-rr = movie_blob_to_dict(ff)
+
+
+
+#ff = load_txt_data(bacon_data_path)
+#rr = write_clean_blob_to_txt(ff,output_path)
+
+a = open(output_path,'r', encoding="utf8")
+aa = a.readlines()
+for i in aa:
+    if "\n" in i:
+        print(i)
