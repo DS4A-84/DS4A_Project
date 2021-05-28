1b222920b53c02ab57ae41bceafd595c2d403579
Oscar Hendrick <lincoln607@gmail.com>
2021-05-27 22:40:30 -0400

    Edited some comments in clean_bacon_data.py

--- clean_bacon_data.py
+++ clean_bacon_data.py
@@ -116,19 +116,21 @@ def create_info_title_pair_list(list):
     return result
 
 
-def create_node_info(list, k_names, filepath):
+def seq_list_to_nominees_df(list, k_names, filepath):
     '''
-    Inputs: nested list of sequential info/title
+    Inputs: list - nested list of sequential info/title
+            k_names - pandas series of kaggle nominee names
+            filepath - an output filepath as a string
 
     this takes the info/title nested list and converts
     to pandas dataframe
     writes outpandas dataframe with 4 columns with each row
     being a name and info on the film they participated
+    only nominees from kaggle dataset are included in this data
 
     returns: nothing
     '''
 
-    #df = pd.DataFrame({'title':[],'year':[],'role':[],'name':[]})
 
     #break down name values to dataframe
     #what if cast is empty
@@ -136,6 +138,8 @@ def create_node_info(list, k_names, filepath):
         t_df=""
         i,t = film
 
+        # create dataframe from t dictionary
+        cast_dir = ""
         if 'cast' in t.keys():
             cast_dir = t['cast']+t['directors']
         else:
@@ -144,16 +148,17 @@ def create_node_info(list, k_names, filepath):
                              'year':[t['year']]*len(cast_dir),
                              'role':['cast']*len(cast_dir),
                              'name':cast_dir
-        })
+                             })
 
-        # fill in director
+        # fill in director role
         t_df.loc[t_df['name'].isin(i['directors']),'role'] = "director"
 
-        # fill in star if it exists
+        # fill in star actor role if it exists in i dictionary
         if 'stars' in i.keys():
             t_df.loc[t_df['name'].isin(i['stars']),'role'] = "star"
 
-
+        # only include row of the dataframe where the names match
+        # the kaggle nominee dataset names
         t_df = t_df[t_df['name'].isin(kaggle_names)]
         if len(t_df)!=0:
             write_to_csv(t_df,filepath)
@@ -169,7 +174,8 @@ def create_node_info(list, k_names, filepath):
 
 def write_to_csv(df,filepath):
     '''
-    input: a pandas DataFrame
+    input: df - a pandas DataFrame
+           filepath - an output filepath as a string
 
     writes to a csv file
     in same diretory as this script
@@ -185,6 +191,8 @@ def write_to_csv(df,filepath):
 # load bacon and kaggle datasets
 raw_bacon_data = load_txt_data(bacon_data_path)
 kaggle_data = load_dataset(kaggle_data_path)
+
+# prepare kaggle nominee names
 kaggle_data = kaggle_data[kaggle_data['category'].isin(categories_interest)]
 kaggle_names = kaggle_data['name'].drop_duplicates()
 
@@ -193,8 +201,10 @@ kaggle_names = kaggle_data['name'].drop_duplicates()
 clean_bacon_data = blob_to_list(raw_bacon_data)
 #print(len(clean_bacon_data))
 #256992
-# convert from list to pandas df
+# convert from non sequential info/title list into sequential nested lists
 bacon_pair_list = create_info_title_pair_list(clean_bacon_data)
 #print(len(bacon_pair_list))
 #123409
-create_node_info(bacon_pair_list, kaggle_names,clean_bacon_df_path)
+# create pandas dataframe of movie info for nominees
+seq_list_to_nominees_df(bacon_pair_list, kaggle_names,clean_bacon_df_path)
+#45964
