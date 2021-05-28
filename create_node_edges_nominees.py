a250732ec6de7f591e800c706617d0a1067458f1
Oscar Hendrick <lincoln607@gmail.com>
2021-05-28 13:28:39 -0400

    modified edges method and create node method
    modified edges and node methods on create_node_edge_nominees.py to get the node and edge csv outputs in the correct format

--- create_node_edges_nominees.py
+++ create_node_edges_nominees.py
@@ -6,19 +6,89 @@
 # on the nominees from the kaggle dataset
 
 
-
+from os import path
 import pandas as pd
 
 
 
 
+
 clean_bacon_df_path = 'data/clean_bacon_df.csv'
+bacon_edges_path = 'data/bacon_edges.csv'
+bacon_nodes_path = 'data/bacon_nodes.csv'
+
 
 
 
 def load_dataset(filepath):
     df = pd.read_csv(filepath)
     return df
+
+
+def write_to_csv(df,filepath):
+    '''
+    input: df - a pandas DataFrame
+           filepath - an output filepath as a string
+
+    writes to a csv file
+    in same diretory as this script
+
+    returns: nothing
+    '''
+    # if no csv exists
+    if not path.exists(filepath):
+        df.to_csv(filepath,index=False)
+    else:
+        df.to_csv(filepath, mode='a', header=False,index=False)
+
+
+def create_edges_csv(df,filepath):
+    '''
+    Inputs: df - pandas dataframe
+            filepath - python string
+
+    this will take the nominees per film title
+    and create link between every nominee in that movie
+    saves output as dataframe with columns source target
+
+    Returns: nothing
+    '''
+
+    films = df['title'].unique()
+
+    for film in films:
+        nominees = df[df['title'] == film]['name']
+        nominees = nominees.reset_index(drop=True)
+
+        # only include films with more than one nominees in it
+        if len(nominees) != 1:
+            for i in range(0, len(nominees)):
+                for j in range(i+1, len(nominees)):
+                    pairs = [[nominees[i], nominees[j]]]
+                    df1 = pd.DataFrame(pairs, columns = ['source', 'target'])
+                    write_to_csv(df1, filepath)
+
+
+def create_nodes_csv(df,filepath):
+    '''
+    Inputs: df - pandas DataFrame of nominee info
+            filepath - string of output path
+
+    This takes the nominee dataframe and converts
+    to a smaller dataframe of two columns
+    name and dictionary of films the nominee was in
+
+    Returns: nothing
+    '''
+
+    nominees = df['name'].unique()
+
+    for nominee in nominees:
+        films = df[df['name']==nominee]
+        films_info = [dict(zip(films.title, zip(films.role,films.year)))]
+
+        df1 = pd.DataFrame({'name':nominee,'films':films_info})
+        write_to_csv(df1, filepath)
 
 
 
@@ -33,34 +103,19 @@ print(a.head())
 4           Blade Runner  1982  director        Ridley Scott
 '''
 
-# to get edges
-# for loop every title and create a new dataframe with columns source target
-# do not include titles with only one nominee in it
-edges = pd.DataFrame({'source':[], 'target':[]})
-films = clean_bacon_df['title'].unique()
+# create edges
+#create_edges_csv(clean_bacon_df,bacon_edges_path)
+# 45451
+'''
+               source              target
+0        Lillian Gish        Donald Crisp
+1       Harrison Ford  Edward James Olmos
+2       Harrison Ford        Ridley Scott
+3  Edward James Olmos        Ridley Scott
+4         Gene Wilder       Madeline Kahn
+'''
 
-for f in films:
-    f = clean_bacon_df[clean_bacon_df['title'] == i]
-    for n in f['name']:
-        x=1
-
-
-
-
-
-
-# to get nodes
+# create nodes
 #per person condence film info into nested dictionary with each dictionary having
 #the nominees film info
-
-
-
-'''
-    index = 0
-    index_end = len(df)
-    for i in range(index,index_end):
-        for j in range(i+1,index_end):
-            pairs = [[df['name'][i],df['name'][j]]]
-            pairs = pd.DataFrame(pairs, columns = ['nomeinee1','nominee2'])
-            write_to_csv(pairs, filepath)
-'''
+create_nodes_csv(clean_bacon_df,bacon_nodes_path)
